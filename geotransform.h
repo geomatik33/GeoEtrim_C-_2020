69e6e88e62b5c3e04efd4a945d1402486efca210
Samet DOGAN <72412141+dogansamet@users.noreply.github.com>
2020-12-13 19:17:43 +0300

    GeoTransform header file added

---
+++ geotransform.h
@@ -0,0 +1,16 @@
+#ifndef GEOTRANSFORM_H
+#define GEOTRANSFORM_H
+
+#include <QWidget>
+
+class Geotransform : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Geotransform(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // GEOTRANSFORM_H
